095d6de888b7f4b0023e981c8e2bd314f027d5f3
BT_1N3_22 <Vincentc270@nycstudents.net>
2025-03-18 12:31:35 -0400

    update

--- BoxOfCandy.java
+++ BoxOfCandy.java
@@ -2,7 +2,9 @@ public class BoxOfCandy
 {
     /** box contains at least one row and is initialized in the constructor. */
     private Candy[][] box;
-
+    public BoxOfCandy(Candy[][] b){
+        box =b;
+    }
     /**
     * Moves one piece of candy in column col, if necessary and possible, so that the box
     * element in row 0 of column col contains a piece of candy, as described in part (a).
@@ -10,14 +12,23 @@ public class BoxOfCandy
     * Precondition: col is a valid column index in box.
     */
     public boolean moveCandyToFirstRow(int col)
-    { /* to be implemented in part (a) */ }
+    { return(false); }
 
     /**
     * Removes from box and returns a piece of candy with flavor specified by the parameter, or
     * returns null if no such piece is found, as described in part (b)
     */
     public Candy removeNextByFlavor(String flavor)
-    { /* to be implemented in part (b) */ }
+    {return(null);}
 
-    // There may be instance variables, constructors, and methods that are not shown.
+    public String toString(){
+        String s= "";
+        for(int row = 0; row < box.length; row++){
+            for(int col = 0; col<box[0].length; col++){
+                s+= box[row][col] + " ";
+            }
+            s+= "\n";
+        }
+        return s;
+    }
 }
--- Candy.java
+++ Candy.java
@@ -1,9 +1,25 @@
+import javax.swing.*;
+
 public class Candy {
+    private String flavor;
+    public static void main(String[] args){
+        Candy[][]box = new Candy[4][3];
+        BoxOfCandy b = new BoxOfCandy(box);
+        box[0][1] = new Candy("lime");
+        box[1][1] = new Candy("orange");
+        box[2][2] = new Candy("cherry");
+        box[3][1] = new Candy("lemon");
+        box[3][2] = new Candy("grape");
+        System.out.println(b);
+    }
+    public Candy(String f){
+        flavor = f;
+    }
 
-    /** Returns a String representing the flavor of this piece of candy */
-    public String getFlavor() {
-        /* implementation not shown */ }
+    public String getFlavor() {return "";}
 
-    // There may be instance variables, constructors, and methods that are not
-    // shown.
+    public String toString(){
+        return flavor;
+    }
+
 }
